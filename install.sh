3f94e368dd1d784d03258d5885a6b8f897173afb
jason211346 <72254883+jason211346@users.noreply.github.com>
2023-04-12 11:09:18 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install -r requirements.txt 
+pip install -U scikit-learn
+pip install tensorboard tensorboardX selenium twder beautifulsoup4 seaborn thop tqdm pytorch_metric_learning openpyxl natsort tensorboard_logger opencv-python pandas seaborn numpy
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install bayesian-optimization==1.4.2
+pip install pycave==3.1.3
+pip install ipykernel --upgrade
+python3 -m ipykernel install --user
