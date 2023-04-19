6af14f7f92155e07679c32a4dfd5d82d538f8524
jason211346 <72254883+jason211346@users.noreply.github.com>
2023-04-19 14:00:43 +0800

    update

--- Fruit_dataset/train_test.sh
+++ Fruit_dataset/train_test.sh
@@ -4,14 +4,14 @@ do
     ckpt=./save/SupCon/fruit_8_models/SupCon_fruit_8_mobilenetv3_large_lr_0.05_decay_0.0001_bsz_128_temp_0.1_trial_0/$seed/ckpt_best.pth
     CUDA_VISIBLE_DEVICES=$gpu_num python3 main.py --batch_size 128 --learning_rate 0.05 --seed $seed --epochs 30
 
-    CUDA_VISIBLE_DEVICES=$gpu_num python3 ExtractedFeatures_plotter.py --random_seed $seed --relabel --embedding_layer shared_embedding --checkpoint_path $ckpt
+    CUDA_VISIBLE_DEVICES=$gpu_num python3 ExtractedFeatures_Plotter.py --random_seed $seed --relabel --embedding_layer shared_embedding --checkpoint_path $ckpt
 
     for NAME in 0 1 2 3 4 5 6 7 
         do
         CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_train.py --seed $seed --ckpt $ckpt --embedding_layer shared_embedding --componentName $NAME --gaussian_num 30
         done
 
-    CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_Features_Ploatter.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding  
+    CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_Features_Plotter.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding  
     CUDA_VISIBLE_DEVICES=$gpu_num python3 BayesOpt_GMM.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding --gaussian_num 30
     CUDA_VISIBLE_DEVICES=$gpu_num python3 Inference.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding --gaussian_num 30
 done
--- Phison_dataset/train_test.sh
+++ Phison_dataset/train_test.sh
@@ -3,7 +3,7 @@ for seed in 1 1212 42
 do
     ckpt=./save/SupCon/phison_models/SupCon_phison_mobilenetv3_large_lr_0.05_decay_0.0001_bsz_128_temp_0.1_trial_0/$seed/ckpt_best.pth
     CUDA_VISIBLE_DEVICES=$gpu_num python3 main.py --batch_size 128 --learning_rate 0.05 --seed $seed --epochs 30
-    CUDA_VISIBLE_DEVICES=$gpu_num python3 ExtractedFeatures_plotter.py --random_seed $seed --relabel --embedding_layer shared_embedding --checkpoint_path $ckpt
+    CUDA_VISIBLE_DEVICES=$gpu_num python3 ExtractedFeatures_Plotter.py --random_seed $seed --relabel --embedding_layer shared_embedding --checkpoint_path $ckpt
       
     for NAME in 4 8 15 18 19 20
         do
@@ -19,7 +19,7 @@ do
         do
         CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_train.py --seed $seed --ckpt $ckpt --embedding_layer shared_embedding --componentName $NAME --gaussian_num 200
         done
-    CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_Features_Ploatter.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding
+    CUDA_VISIBLE_DEVICES=$gpu_num python3 GMM_Features_Plotter.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding
     CUDA_VISIBLE_DEVICES=$gpu_num python3 BayesOpt_GMM.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding
     CUDA_VISIBLE_DEVICES=$gpu_num python3 Inference.py --batch_size 1024 --seed $seed --relabel --ckpt $ckpt --embedding_layer shared_embedding
 done
